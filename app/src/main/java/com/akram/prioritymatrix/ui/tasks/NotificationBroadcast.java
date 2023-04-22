1cc27054ebb171aee5cc2e42908b31912add7676
AkramHS00 <akramhs@hotmail.com>
2023-04-23 00:09:19 +0100

    Added intent and pending intent which allows user to click notification to open the app.

--- app/src/main/java/com/akram/prioritymatrix/ui/tasks/NotificationBroadcast.java
+++ app/src/main/java/com/akram/prioritymatrix/ui/tasks/NotificationBroadcast.java
@@ -1,5 +1,6 @@
 package com.akram.prioritymatrix.ui.tasks;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +9,7 @@ import android.util.Log;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.akram.prioritymatrix.MainActivity;
 import com.akram.prioritymatrix.R;
 
 public class NotificationBroadcast extends BroadcastReceiver {
@@ -20,13 +22,20 @@ public class NotificationBroadcast extends BroadcastReceiver {
         String taskTitle = intent.getStringExtra("taskTitle");
         long intentId = intent.getLongExtra("intentId",0);
 
+        //New intent to open the app when user clicks the notification
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        notificationIntent.putExtra("intentId", intentId);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, (int) intentId, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
         //Set notification properties
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "task_reminder")
                 .setSmallIcon(R.drawable.matrix_icon)
                 .setContentTitle("Task Reminder")
                 .setContentText(taskTitle)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
 
         //Display notification, set ID to 0 as we cancel the alarm manager not the notification when updating tasks
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
